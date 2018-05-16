e7d5da95a9f7c657e0f401ad52d5677a679591d8
amoodie <amoodie@rice.edu>
2018-05-16 13:04:30 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wwo ripper
+
+script for ripping the met data for a location each day
---
+++ src/WWO_ripper.m
@@ -0,0 +1,13 @@
+function WWO_ripper()
+% this script will download the data from WWO
+
+%% authentication
+apikey_fid = fopen(fullfile('src', 'key.txt'));
+apikey = fgetl(apikey_fid);
+
+
+
+%% parameters
+
+
+end

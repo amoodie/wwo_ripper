4b59489d078249e9e69bbbe431bb9a07c64385a5
amoodie <amoodie@rice.edu>
2018-06-01 11:08:21 +0800

    add some documentation to the file, and delete extraneous functions

--- src/WWO_ripper.m
+++ src/WWO_ripper.m
@@ -1,6 +1,20 @@
 function WWO_ripper()
-    % this script will download the data from WWO
+    %WWO_ripper collects hour location specific data from the WWO historical api.
+    %
+    % This script is useful for scraping meteorological data from the web 
+    % for a single location. Note that the quality or source of data is 
+    % unknown, so this approach should be used only if no science-ready 
+    % data sources exist.
+    %
+    % See the readme at GitHub source for complete instructions on use.
+    %   https://github.com/amoodie/wwo_ripper
+    % 
+    % In summary, you must first obtain an API key from the WWO website. 
+    % Then, edit the parameters as needed below to set up the API calls.
+    % Finally, run this script daily until all data are aquired.
+    %
 
+    
     %% authentication
     apikey_fid = fopen(fullfile('src', 'key.txt'));
     apikey = fgetl(apikey_fid);
@@ -23,7 +37,7 @@ function WWO_ripper()
 
     %% get dates to hit on this script run
     % method to find all missing dates and do the most recent n
-    n = 1; % set to 2 for testing, 450 for real running
+    n = 450; % set to 2 for testing, 450 for real running
     datesall = (datenum('2008-01-01'):today-1)';
     [~, fileshave] = listmats(fullfile('..', 'output'));
     [filessplit] = cellfun(@(x) strsplit(x, '_'), fileshave, 'Unif', 0);
@@ -33,16 +47,7 @@ function WWO_ripper()
     datesdonthave = datesall(~dateshave_log);
     ntoidx = min([n, length(datesdonthave)]);
     dates = datesdonthave(end-ntoidx+1:end);
-
-    % method for pulling from file
-    % n = 450; % set to 2 for testing, 450 for real running
-    % lastdate_new = lastdate-n;
-    % dates = lastdate-1:-1:lastdate_new;
-
-    % method for specific calls
-    % dates_str = {'2016-01-27'};
-    % dates = cellfun(@datenum, dates_str);
-    % n = length(dates);
+    
 
     %% preallocate
     errorlist = {};
@@ -75,6 +80,7 @@ function WWO_ripper()
 
     end
 
+    
     %% print out errored
     if ~isempty(errorlist)
         disp('Error collection for dates:')
@@ -90,25 +96,5 @@ function WWO_ripper()
     fprintf(lastdate_fid, '%d', lastdate_new);
     fclose(lastdate_fid);
 
-end
-
-
-function [str] = format_apicall(baseapi, key, q, varargin)
-    % format to a proper api call
-    % requires baseapi string, key, and location (q)
-    % takes the supported fields (see below) as options
     
-    ip = inputParser();
-    addRequired(ip, 'baseapi', @ischar);
-    addRequired(ip, 'key', @ischar);
-    addRequired(ip, 'q', @ischar);
-    addParameter(ip, 'date', @ischar);
-    addParameter(ip, 'tp', @ischar);
-    addParameter(ip, 'format', @ischar);
-    
-    parse(ip, baseapi, key, q, varargin{:});
-    
-%     a = p.Results.width*p.Results.height; 
-
-%     str = [ip.Results.baseapi
 end
